295354d07b7e8d160e7ca5b2a8d8eddbfd35a0e6
Ayan Chakrabarti <ayan@wustl.edu>
2020-05-08 02:12:20 -0500

    .

---
+++ js/timer.js
@@ -0,0 +1,25 @@
+var theTime=0, timer=-1;
+
+const dispTime = function() {
+    secs = theTime%60;
+    mins = (theTime-secs)/60;
+    fmstr = ("00" + mins).slice(-2) + ":" + ("00" + secs).slice(-2);
+    $('#time').html(fmstr);
+}
+
+const countDown = function() {
+    if(theTime <= 0) {timer=-1; return;}
+    theTime--;
+    dispTime();
+
+    if(theTime > 0)
+	timer=setTimeout(countDown,1000);
+    else
+	$('#bell')[0].play();
+}
+
+const stTimer = function(id) {
+    if(timer != -1) clearTimeout(timer); $('#bell')[0].pause();
+    theTime = id; dispTime();
+    timer=setTimeout(countDown,1000);
+}
